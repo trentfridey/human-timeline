0db6e90b61e931d521eb43185b5c23c0fb3c666b
trentfridey <tfri.92@gmail.com>
2020-12-28 00:14:06 -0500

    add styles, tracker, update yaxis labels, add horizontal grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 
 import React from "react";
 import tz from 'timezone';
-import {TimeRange, TimeSeries } from 'pondjs'
-import { Charts, ChartContainer, ChartRow, YAxis, LineChart } from 'react-timeseries-charts'
+import { TimeRange, TimeRangeEvent, TimeSeries } from 'pondjs'
+import { 
+  Charts, 
+  ChartContainer, 
+  ChartRow, 
+  YAxis, 
+  LineChart,
+  TimeMarker, 
+  styler,
+  CrossHairs,
+  Legend
+} from 'react-timeseries-charts'
+
+const days = 24*60*60*1000
 
 class App extends React.Component {
   constructor (props){
     super(props);
-    const days = 24*60*60*1000
-    const window = 115*days
-    const defaultTimeRange = [Date.now()-window, Date.now()+window]
-    this.location = {lat: 35.835, lng: -78.783 }
+    this.generateSunSeries = this.generateSunSeries.bind(this)
+    this.handleTimeRangeChange = this.handleTimeRangeChange.bind(this)
+    this.handleTrackerChanged = this.handleTrackerChanged.bind(this)
     this.calculateSunriseSunset = this.calculateSunriseSunset.bind(this)
+    this.handleMouseMove = this.handleMouseMove.bind(this)
 
+    const window = 15*days
+    const defaultTimeRange = [Date.now()-window, Date.now()+window]
+    this.location = { lat: 35.835, lng: -78.783 }
+    const sunSeries = this.generateSunSeries(new TimeRange(defaultTimeRange))
+    this.state = { sunSeries, timeRange: new TimeRange(defaultTimeRange), x: null, y: null, tracker: null}
+
+  }
+  generateSunSeries (timeRange) {
     const sunEvents = []
-    for(let i = defaultTimeRange[0]; i <= defaultTimeRange[1]; i += 1000 * 60 * 60 * 24) {
+    const start = timeRange.toJSON()[0]
+    const end = timeRange.toJSON()[1]
+    for(let i = start; i <= end; i += 1*days) {
       const { sunrise, sunset } = this.calculateSunriseSunset(new Date(i))
       sunEvents.push([i,sunrise,sunset])
     }
     const sunSeries = new TimeSeries({ name: 'sunrises', columns: ["time","sunrise", "sunset"], points: sunEvents})
-    this.state = { sunSeries, timeRange: defaultTimeRange }
-
+    return sunSeries
+  }
+  handleTimeRangeChange(timeRange){
+    const newSeries = this.generateSunSeries(timeRange)
+    this.setState({ timeRange, sunSeries: newSeries })
+  }
+  handleTrackerChanged(tracker) {
+    if (!tracker) {
+        this.setState({ tracker, x: null, y: null });
+    } else {
+        this.setState({ tracker });
+    }
+  }
+  handleMouseMove (x, y)  {
+    this.setState({ x, y });
   }
   calculateSunriseSunset (date) {
     const { lat, lng } = this.location
@@ -60,35 +95,23 @@ class App extends React.Component {
   
 
   render() {
-    const style = {
-      sunset: {
-          line: {
-              normal: {stroke: "steelblue", fill: "none", strokeWidth: 1},
-              highlighted: {stroke: "#5a98cb", fill: "none", strokeWidth: 1},
-              selected: {stroke: "steelblue", fill: "none", strokeWidth: 1},
-              muted: {stroke: "steelblue", fill: "none", opacity: 0.4, strokeWidth: 1}
-          },
-      },
-      sunrise: {
-          line: {
-            normal: {stroke: "gold", fill: "none", strokeWidth: 1},
-            highlighted: {stroke: "#5a98cb", fill: "none", strokeWidth: 1},
-            selected: {stroke: "gold", fill: "none", strokeWidth: 1},
-            muted: {stroke: "gold", fill: "none", opacity: 0.4, strokeWidth: 1}
-        },
-      }
-  };
+    const style = styler([
+      {key: 'sunset', color: "steelblue", width: 2}, 
+      {key: 'sunrise', color: "goldenrod", width: 2}
+    ])
     return (
       <>
         <div>
-          <ChartContainer title={"Timeline"} showGrid={true} enablePanZoom={true} timeRange={new TimeRange(this.state.timeRange)} width={800}>
-            <ChartRow height="200" enablePanZoom={true}>
-              <YAxis id="axis1" label="hours" min={0} max={24} width="60" type="linear"/>
+          <ChartContainer trackerPosition={this.state.tracker} onMouseMove={(x, y) => this.handleMouseMove(x, y)} onTrackerChanged={this.handleTrackerChanged} title={"Timeline"} showGrid={true} enablePanZoom={true} timeRange={this.state.timeRange} width={800} onTimeRangeChanged={this.handleTimeRangeChange}>
+            <ChartRow height="400">
+              <YAxis format={'.2s'} tickCount={25} showGrid={true} id="axis1" label="hours" min={24} max={0} width="60" type="linear"/>
               <Charts>
-                <LineChart axis="axis1" style={style} columns={["sunrise","sunset"]} series={this.state.sunSeries} />
+                <LineChart info={[{label: 't', value: 2}]} axis="axis1" style={style} columns={["sunrise","sunset"]} series={this.state.sunSeries} />
+                <TimeMarker axis="axis1" time={new Date()} infoStyle={{line: {strokeWidth: "2px", stroke: "#83C2FC"}}} />
               </Charts>
             </ChartRow>
           </ChartContainer>
+          <Legend type="line" align="right" style={style} categories={[{key: 'sunrise', label: 'sunrise'},{key: 'sunset', label:'sunset'}]}/>
         </div>
       </>
     );
