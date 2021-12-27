4cbe9e89fc3c60331a61e20fdcf43ad3d75d81ef
trentfridey <tfri.92@gmail.com>
2021-12-27 15:10:59 -0500

    add Geolocation API integration
    removed DST markers, added current time baseline indicator, added
    buttons for navigation, removed delta indicator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { calculateSunriseSunset } from './util/calcSun' 
-import { TimeRange, TimeRangeEvent, TimeSeries } from 'pondjs'
+import { TimeRange, Time, TimeSeries } from 'pondjs'
 import { 
 	Resizable,
 	Charts, 
@@ -11,8 +11,6 @@ import {
   LineChart,
   TimeMarker, 
   styler,
-  CrossHairs,
-  Legend,
   Baseline,
 } from 'react-timeseries-charts'
 
@@ -28,14 +26,16 @@ class App extends React.Component {
     this.handleTimeRangeChange = this.handleTimeRangeChange.bind(this)
     this.handleTrackerChanged = this.handleTrackerChanged.bind(this)
     this.handleMouseMove = this.handleMouseMove.bind(this)
-
+    this.onObtainLocation = this.onObtainLocation.bind(this)
+    this.handleNav = this.handleNav.bind(this)
+    
     const window = 240*days
     const defaultTimeRange = [Date.now()-window/2, Date.now()+3*window/2]
-    this.location = { lat: 35.835, lng: -78.783 }
-    const sunSeries = this.generateSunSeries(new TimeRange(defaultTimeRange))
+    const initialLocation = { lat: 35.835, lng: -78.783 }
+    const sunSeries = this.generateSunSeries(initialLocation, new TimeRange(defaultTimeRange))
     
     this.state = { 
-      sunSeries, 
+      sunSeries: sunSeries, 
       solstice: { summer: 0, winter: 0 },
       equinox: [],
       sunrise: { max: 0, min: 0},
@@ -44,6 +44,7 @@ class App extends React.Component {
       x: null, 
       y: null, 
       tracker: null,
+      location: initialLocation,
       markers: {
         solstice: true,
         equinox: true,
@@ -53,12 +54,22 @@ class App extends React.Component {
     this.getSolstice()
     this.getEquinox()
   }
-  generateSunSeries (timeRange) {
+  componentDidMount () {
+    navigator.geolocation.getCurrentPosition(this.onObtainLocation, console.log, {enableHighAccuracy: true})
+  }
+  onObtainLocation ({ coords }){
+    const location = { lat: coords.latitude, lng: coords.longitude}
+    const sunSeries = this.generateSunSeries(location, this.state.timeRange)
+    this.setState({ sunSeries, location })
+    this.getSolstice()
+    this.getEquinox()
+  }
+  generateSunSeries (location, timeRange) {
     const sunEvents = []
     const start = timeRange.toJSON()[0]
     const end = timeRange.toJSON()[1]
     for(let i = start; i <= end; i += 1*days) {
-      const { sunrise, sunset } = calculateSunriseSunset(this.location, new Date(i))
+      const { sunrise, sunset } = calculateSunriseSunset(location, new Date(i))
       const hoursOfSun = sunset - sunrise
       const midday = sunrise + (hoursOfSun) / 2
       sunEvents.push([i, sunrise, midday, sunset, hoursOfSun])
@@ -81,6 +92,13 @@ class App extends React.Component {
   }
   handleMouseMove (x, y)  {
     this.setState({ x, y });
+  }
+  handleNav(delta) {
+    const date = this.state.tracker ? this.state.tracker : new Date()
+    const currentDay =  date.getDate()
+    const currentDate = [date.getFullYear(), date.getMonth()]
+    let nextDate = new Date(...currentDate, currentDay+delta ) 
+    this.setState({ tracker: nextDate }) 
   }
   formatRelativeTicks (d) {
     if(!d) return 'None'
@@ -125,9 +143,8 @@ class App extends React.Component {
   getEquinox () {
     for(const evt of this.state.sunSeries.events()){
       const hoursOfSun = evt.get('hoursOfSun')
-      if(Math.abs(hoursOfSun - 12) < 0.01) this.state.equinox = [evt.begin(), ...this.state.equinox]
+      if(Math.abs(hoursOfSun - 12) < 0.02) this.state.equinox = [evt.begin(), ...this.state.equinox]
     }
-    console.log(this.state.equinox)
   }
 
   render() {
@@ -136,27 +153,49 @@ class App extends React.Component {
       {key: 'midday', color: 'orange', width: 5},
       {key: 'sunrise', color: "gold", width: 5}
     ])
-    const hoverDate = this.state.tracker ? this.state.tracker : new Date()
+    const now = new Date()
+    const hoverDate = this.state.tracker ? this.state.tracker : now
     const index = this.state.sunSeries.bisect(hoverDate)
     const trackerEvent = this.state.sunSeries.at(index);
     const sunrise = trackerEvent.get('sunrise');
     const midday = trackerEvent.get('midday');
     const sunset = trackerEvent.get('sunset');
-    const hoursOfDaylight = (sunset - sunrise)
+    const hoursOfDaylight = (sunset - sunrise);
+    const nowHours = now.getHours() + now.getMinutes()/60 + now.getSeconds()/(60*60)
+    const remainingHours = sunset > nowHours ? sunset - nowHours : 0
     const riseLabel = this.fractionalHoursToHoursMinutes(sunrise)
     const middayLabel = this.fractionalHoursToHoursMinutes(midday)
     const setLabel = this.fractionalHoursToHoursMinutes(sunset-12)
+    const baselineStyle = {
+      line: {
+          stroke: "black",
+          strokeWidth: 2,
+          opacity: 1,
+          strokeDasharray: "none"
+      }
+    };
+    const h2Style = { padding: 10, margin: 'none', textAlign: 'center'}
+    const buttonStyle = { 
+      fontWeight: 'bold', 
+      fontSize: '20px',
+      padding: 20, 
+      flex: '1 0 auto', 
+      textAlign: 'left', 
+      cursor: 'pointer',
+      border: 'none'
+    }
     return (
     <>
       <div style={{ textAlign: 'center', fontSize: '45px'}}>
         🌞
       </div>
       <div>
-        <h2 style={{ textAlign: 'center', backgroundColor: 'white', padding: 10, border: '5px solid black' }}>Date....{hoverDate.getMonth() + 1}/{hoverDate.getDate()}/{hoverDate.getFullYear()}</h2>
-        <h2 style={{ textAlign: 'center', backgroundColor: 'gold', margin: 'none', padding: 10}}>Sunrise...{riseLabel}AM</h2>
-        <h2 style={{ textAlign: 'center', backgroundColor: 'orange', margin: 'none', padding: 10}}>Midday......{middayLabel}</h2>
-        <h2 style={{ textAlign: 'center', backgroundColor: 'steelblue', margin: 'none', padding: 10, color: "white"}}>Sunset....{setLabel}PM</h2>
-        <div style={{ fontFamily: 'monospace', textAlign: 'right' }}>Hours of Daylight: { this.formatHours(hoursOfDaylight) }</div>
+        <h2 style={{ ...h2Style, backgroundColor: 'white', border: '5px solid black' }}>Date....{hoverDate.getMonth() + 1}/{hoverDate.getDate()}/{hoverDate.getFullYear()}</h2>
+        <h2 style={{ ...h2Style, backgroundColor: 'gold' }}>Sunrise...{riseLabel}AM</h2>
+        <h2 style={{ ...h2Style, backgroundColor: 'orange'}}>Midday......{middayLabel}</h2>
+        <h2 style={{ ...h2Style, backgroundColor: 'steelblue', color: "white"}}>Sunset....{setLabel}PM</h2>
+        <h2 style={{ ...h2Style, border: '5px solid black' }} >Daylight Hours: { this.formatHours(hoursOfDaylight) }</h2>
+        <h2 style={{ ...h2Style, border: '5px solid black', borderTop: 'none' }} >Remaining Hours: {this.formatHours(remainingHours)}</h2>
       </div>
       <div style={{width: '100%', border: '1px solid black', overflow: 'hidden'}}>
         <Resizable>
@@ -175,6 +214,8 @@ class App extends React.Component {
               <YAxis format={'.2s'} tickCount={25} showGrid={true} id="axis1" min={24} max={0} width="20" type="linear"/>
               <Charts>
                 <LineChart info={[{label: 't', value: 2}]} axis="axis1" style={style} columns={["sunrise",'midday',"sunset"]} series={this.state.sunSeries} />
+                
+                <Baseline axis="axis1" value={nowHours} style={baselineStyle}/>
                 <Baseline axis="axis1" value={this.state.sunSeries.min('sunrise')}  position="right"/>
                 <Baseline axis="axis1" value={this.state.sunSeries.max('sunrise')}  position="right"/>
                 <Baseline axis="axis1" value={this.state.sunSeries.min('sunset')}   position="right"/>
@@ -183,34 +224,31 @@ class App extends React.Component {
                 <Baseline axis="axis1" value={this.state.sunSeries.avg('sunset')}   position="right"/>
 
                 <TimeMarker axis="axis1" time={new Date()} label="today" infoStyle={{line: {strokeWidth: "2px", stroke: "black"}}} />
-
                 <TimeMarker axis="axis1" time={this.state.sunrise.max} infoStyle={{ line: { strokeWidth: "2px", stroke: 'gold' }}}/>
                 <TimeMarker axis="axis1" time={this.state.sunrise.min} infoStyle={{ line: { strokeWidth: "2px", stroke: 'gold' }}}/>
                 <TimeMarker axis="axis1" time={this.state.sunset.max} infoStyle={{ line: { strokeWidth: "2px", stroke: 'steelblue' }}}/>
                 <TimeMarker axis="axis1" time={this.state.sunset.min} infoStyle={{ line: { strokeWidth: "2px", stroke: 'steelblue' }}}/>
-
                 <TimeMarker axis="axis1" time={this.state.solstice.summer} infoStyle={{ line: { strokeWidth: "2px", stroke: 'orange' }}} />
                 <TimeMarker axis="axis1" time={this.state.solstice.winter} infoStyle={{ line: { strokeWidth: "2px", stroke: 'orange' }}} />
 
                 {this.state.equinox.map(e => {
                   return <TimeMarker axis='axis1' time={e} infoStyle={{line: { strokeWidth: "2px", stroke: 'grey' }}} />
                 })}
-
-                {/* Need to have these events plot on defintion: second sunday in march & first sunday in november; auto-repeat */}
-                <TimeMarker label="DST starts" axis="axis1" time={new Date('2021/03/14 01:00:00')} infoStyle={{ line: {strokeWidth: "2px", stroke: "pink"}}} />
-                <TimeMarker label="DST ends" axis="axis1" time={new Date('2020/11/01 02:00:00')} infoStyle={{ line: {strokeWidth: "2px", stroke: "lightblue"}}} />
-                
               </Charts>
             </ChartRow>
 				  </ChartContainer>
 				</Resizable>
         </div>
           <div style={{ fontFamily: 'monospace'}}>
-            Location: { `Lat: ${this.location.lat}, Long: ${this.location.lng}` }
-            <br/>
-            Today: { `${new Date().toDateString()}` }
-            <br/>
-            Delta: { `${this.formatRelativeTicks(this.state.tracker)}` }
+          <div style={{display: 'flex'}}>
+            <button onClick={()=>this.handleNav(-1)} style={{ ...buttonStyle,  backgroundColor: 'steelblue',  borderBottom: '5px solid blue'  }} >
+            { '< Yesterday' }
+            </button>
+            <button onClick={()=>this.handleNav(1)} style={{ ...buttonStyle, backgroundColor: 'gold',  textAlign: 'right', borderBottom: '5px solid orange' }} >
+              { 'Tomorrow  >' }
+            </button>
+          </div>
+            Location: { `Lat: ${this.state.location.lat}, Long: ${this.state.location.lng}` }
             <br/>
           </div>
 
